9dd19cbbf363261fb1d60e71fbb5291181a100f1
Gustavo Alves da Silva <gustavo_csharp@outlook.com>
2015-05-21 11:47:41 -0300

    Criação Formulário Principal

---
+++ SistemaControle/SistemaControle/src/com/gustavo/sistemacontrole/formularios/FormPrincipal.java
@@ -0,0 +1,121 @@
+
+package com.gustavo.sistemacontrole.formularios;
+
+public class FormPrincipal extends javax.swing.JFrame {
+
+    public FormPrincipal() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sistema de Vendas");
+        setPreferredSize(new java.awt.Dimension(800, 600));
+
+        jMenu1.setText("Cadastros");
+
+        jMenuItem1.setText("Estados");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Cidades");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Bairros");
+        jMenu1.add(jMenuItem3);
+
+        jMenuItem4.setText("Telefones");
+        jMenu1.add(jMenuItem4);
+
+        jMenuItem5.setText("Clientes");
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem6.setText("Fornecedores");
+        jMenu1.add(jMenuItem6);
+
+        jMenuItem7.setText("Produtos");
+        jMenu1.add(jMenuItem7);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Compra");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Venda");
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Relatórios");
+        jMenuBar1.add(jMenu4);
+
+        jMenu5.setText("Sair");
+        jMenu5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu5MouseClicked(evt);
+            }
+        });
+        jMenuBar1.add(jMenu5);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 579, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(800, 600));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenu5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu5MouseClicked
+            
+        System.exit(0);
+        
+    }//GEN-LAST:event_jMenu5MouseClicked
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FormPrincipal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ SistemaControle/SistemaControle/src/com/gustavo/sistemacontrole/principal/Principal.java
@@ -0,0 +1,14 @@
+package com.gustavo.sistemacontrole.principal;
+import com.gustavo.sistemacontrole.formularios.FormPrincipal;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        FormPrincipal principal = new FormPrincipal();
+        
+        principal.setVisible(true);
+        
+    }
+    
+}
